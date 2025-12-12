ac3039ae98fa7d1d66ed91ae64e5cb3d857ef074
RyanMJones89 <rmjones@student.fullsail.edu>
2025-12-12 15:12:16 -0500

    Type Casting

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -6,7 +6,16 @@
 #include <cstdint>
 
 int main(){
-    
-    std::cout << std::endl << std::endl;
+    float flt;
+    int32_t sgn;
+    uint32_t unsgn;
+
+    flt = -7.66;
+    sgn = flt;
+    unsgn = sgn; // this will run but will not provide correct inforatmion because an unsigned int can't be converted to a signed in 
+
+    std::cout << "float: " << std::endl;
+    std::cout << "int32: " << std::endl;
+    std::cout << "uint32: " << std::endl;
     return (0);
 }
