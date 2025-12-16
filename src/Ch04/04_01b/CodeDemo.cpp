9528ba3b09e9e2cfb8738a0c9f98a78824391925
RyanMJones89 <rmjones@student.fullsail.edu>
2025-12-16 13:42:40 -0500

    if statements

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -5,7 +5,37 @@
 #include <iostream>
 
 int main(){
-    
+    int a = 1023;
+    bool flag = flase;
+    char ltter = 'd';
+
+    if (a > 1000)
+    {
+        std::cout << "Warning: a is over 1000." << std::endl;
+    }
+
+    if (a % 2) {
+        std::cout << "a is odd" << std::endl;
+    }
+    else
+    {
+        std::cout << "a is even" << std::endl;
+    }
+
+    std::cout << "The letter " << ltter << " is ";
+    if (ltter != 'a' && ltter != 'e' && ltter != 'i' && lttr != 'o' && lttr != 'u' && ltter != 'A' && lttr != "I" && lttr != "O" && lttr != "U") {
+        std::cout << "not ";
+    }
+    std::cout << "a vowel" << std::endl;
+
+    if (flag) {
+        std::cout << "The flag is true!" << std::endl;
+    }
+    else
+    {
+        std::cout << "The flag is false!" << std::endl;
+    }
+
     std::cout << std::endl << std::endl;
     return (0);
 }
